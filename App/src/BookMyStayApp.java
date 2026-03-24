afea11be832e0e8dccf4d2cf125ede1a77545a97
dheeraj408 <abhidheeraj6@gmail.com>
2026-03-24 15:25:39 +0530

    [dheeraj408] Added UC3

--- App/src/BookMyStayApp.java
+++ App/src/BookMyStayApp.java
@@ -1,15 +1,3 @@
-public class BookMyStayApp {
 
-    public static void main(String[] args) {
-        // Print a welcome message to the console
-        System.out.println("Welcome to the Hotel Booking System!");
-
-        // Display application name and version information
-        System.out.println("Application: Hotel Booking System");
-        System.out.println("Version: 1.0");
-
-        // Program terminates after printing messages
-    }
-}
 
 
